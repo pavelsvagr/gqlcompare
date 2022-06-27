7a3c46f19761ec209002d41f53721ba5393957b4
pavelsvagr <pavel.svagr@ackee.cz>
2022-06-27 18:41:22 +0200

    🐛 Load slack notifications config

--- src/loadConfig.ts
+++ src/loadConfig.ts
@@ -64,9 +64,15 @@ const remapConfig = (config: Record<string, any>) => {
     },
     notifications: {
       slack: {
-        url: replaceWithEnv(config.settings.url, 'GQL_COMPARE_SLACK_URL'),
+        url: replaceWithEnv(
+          config.notifications?.slack?.url,
+          'GQL_COMPARE_SLACK_URL'
+        ),
         mentions: readArray(
-          replaceWithEnv(config.settings.mentions, 'GQL_COMPARE_SLACK_MENTIONS')
+          replaceWithEnv(
+            config.notifications?.slack?.mentions,
+            'GQL_COMPARE_SLACK_MENTIONS'
+          )
         ),
       },
     },
